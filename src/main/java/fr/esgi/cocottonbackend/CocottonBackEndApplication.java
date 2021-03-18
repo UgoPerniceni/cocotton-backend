a81fe5f7ec87810a6cbd88d4b9fca2e3755d6870
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-03-18 12:26:39 +0100

    initialize spring boot :tada:

---
+++ src/main/java/fr/esgi/cocottonbackend/CocottonBackEndApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.cocottonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocottonBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocottonBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/cocottonbackend/CocottonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.cocottonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocottonBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
