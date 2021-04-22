3a2488d58ca9eeacf53de7d97ca7a24435d72ddc
Benoît Valle <benoit.valleea@hotmail.fr>
2021-04-22 23:27:09 +0200

    add application recipe

--- src/main/java/fr/esgi/cocotton/application/recipe/FindRecipeById.java
+++ src/main/java/fr/esgi/cocotton/application/recipe/FindRecipeById.java
@@ -1,4 +1,21 @@
 package fr.esgi.cocotton.application.recipe;
 
+import fr.esgi.cocotton.domain.models.recipe.Recipe;
+import fr.esgi.cocotton.domain.models.recipe.RecipeDao;
+import fr.esgi.cocotton.infrastructure.common.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class FindRecipeById {
+
+    private final RecipeDao recipeDao;
+
+    public FindRecipeById(RecipeDao recipeDao){
+        this.recipeDao = recipeDao;
+    }
+    public Recipe execute(String id) {
+        return recipeDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("recipe", id));
+    }
 }
