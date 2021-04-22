e8a86a1387a4faa656104c98cf5d8db010317482
Benoît Valle <benoit.valleea@hotmail.fr>
2021-04-22 23:27:51 +0200

    add infrastructure recipe

--- src/main/java/fr/esgi/cocotton/infrastructure/recipe/persistence/JpaRecipeDao.java
+++ src/main/java/fr/esgi/cocotton/infrastructure/recipe/persistence/JpaRecipeDao.java
@@ -1,11 +1,17 @@
 package fr.esgi.cocotton.infrastructure.recipe.persistence;
 
+import fr.esgi.cocotton.application.user.FindByIdUser;
 import fr.esgi.cocotton.domain.models.recipe.RecipeDao;
 import fr.esgi.cocotton.domain.models.recipe.Recipe;
 import fr.esgi.cocotton.domain.models.user.User;
+import fr.esgi.cocotton.infrastructure.common.exception.ResourceNotFoundException;
+import fr.esgi.cocotton.infrastructure.user.persistance.JpaUser;
+import fr.esgi.cocotton.infrastructure.user.persistance.JpaUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,10 +20,12 @@ import java.util.stream.Collectors;
 public class JpaRecipeDao implements RecipeDao{
 
     private final JpaRecipeRepository repository;
+    private final JpaUserRepository jpaUserRepository;
 
     @Autowired
-    public JpaRecipeDao(JpaRecipeRepository repository){
+    public JpaRecipeDao(JpaRecipeRepository repository, JpaUserRepository jpaUserRepository){
         this.repository = repository;
+        this.jpaUserRepository = jpaUserRepository;
     }
 
     public List<Recipe> findAll() {
@@ -28,32 +36,32 @@ public class JpaRecipeDao implements RecipeDao{
                 .collect(Collectors.toList());
     }
 
-    @Override
     public Optional<Recipe> findById(String id) {
         Optional<JpaRecipe> jpaRecipe = repository.findById((id));
         return jpaRecipe.map(recipe -> new Recipe(recipe.getId(), recipe.getName(),
                 new User(recipe.getUser().getId(), recipe.getUser().getFirstName(), recipe.getUser().getLastName(), recipe.getUser().getEmail(), recipe.getUser().getPassword(), recipe.getUser().getGender(), recipe.getUser().getBirthDate())));
     }
 
-    @Override
     public List<Recipe> findAllByUser(User user) {
 //        return repository.findAllByUser(user);
         return null;
     }
 
-    @Override
     public List<Recipe> findAllByNameLike(String name) {
 //        return repository.findBy;
         return null;
     }
 
-    @Override
     public String save(Recipe recipe) {
-        return null;
+        JpaUser jpaUser = jpaUserRepository.findById(recipe.getUser().getId()).orElseThrow(() -> new ResourceNotFoundException("user for recipe", recipe.getId()));
+        JpaRecipe jpaRecipe = new JpaRecipe(recipe.getId(), recipe.getName(), jpaUser);
+//        jpaUser.getR
+        repository.save(jpaRecipe);
+        return jpaRecipe.getId();
     }
 
-    @Override
+    @Transactional
     public void deleteById(String id) {
-
+        repository.deleteById(id);
     }
 }
