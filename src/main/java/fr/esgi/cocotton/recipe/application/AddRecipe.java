31221d1a0c5b4eb2ac914dcc2a783569326e4dc5
Kelig MARTIN <kmartin@wedigital.garden>
2021-07-04 15:49:16 +0200

    fix(recipe): add userId to recipe on POST

--- src/main/java/fr/esgi/cocotton/recipe/application/AddRecipe.java
+++ src/main/java/fr/esgi/cocotton/recipe/application/AddRecipe.java
@@ -1,5 +1,7 @@
 package fr.esgi.cocotton.recipe.application;
 
+import fr.esgi.cocotton.profile.application.FindProfileFromToken;
+import fr.esgi.cocotton.profile.domain.Profile;
 import fr.esgi.cocotton.recipe.domain.Recipe;
 import fr.esgi.cocotton.recipe.domain.RecipeDao;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,16 @@ import org.springframework.stereotype.Service;
 public class AddRecipe {
 
     private final RecipeDao recipeDao;
+    private final FindProfileFromToken findProfileFromToken;
 
-    public AddRecipe(RecipeDao recipeDao) {
+    public AddRecipe(RecipeDao recipeDao, FindProfileFromToken findProfileFromToken) {
         this.recipeDao = recipeDao;
+        this.findProfileFromToken = findProfileFromToken;
     }
 
-    public String execute(Recipe recipe) {
+    public String execute(Recipe recipe, String token) {
+        Profile profile = findProfileFromToken.execute(token);
+        recipe.setUserId(profile.getId());
         return recipeDao.save(recipe);
     }
 }
