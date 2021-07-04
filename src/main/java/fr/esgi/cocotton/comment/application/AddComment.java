627ab06fa10ce102f98aff81d5565b75a88466cb
Kelig MARTIN <kmartin@wedigital.garden>
2021-07-04 17:36:33 +0200

    fix(comment): add userId from Token

--- src/main/java/fr/esgi/cocotton/comment/application/AddComment.java
+++ src/main/java/fr/esgi/cocotton/comment/application/AddComment.java
@@ -2,18 +2,26 @@ package fr.esgi.cocotton.comment.application;
 
 import fr.esgi.cocotton.comment.domain.Comment;
 import fr.esgi.cocotton.comment.domain.CommentDao;
+import fr.esgi.cocotton.profile.application.FindProfileFromToken;
+import fr.esgi.cocotton.profile.domain.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddComment {
 
     private final CommentDao commentDao;
+    private final FindProfileFromToken findProfileFromToken;
 
-    public AddComment(CommentDao commentDao) {
+    @Autowired
+    public AddComment(CommentDao commentDao, FindProfileFromToken findProfileFromToken) {
         this.commentDao = commentDao;
+        this.findProfileFromToken = findProfileFromToken;
     }
 
-    public String execute(Comment comment) {
+    public String execute(Comment comment, String token) {
+        Profile profile = findProfileFromToken.execute(token);
+        comment.setUserId(profile.getId());
         return commentDao.save(comment);
     }
 }
