d26818891779ea4b4c2e67de4fa4b58168cf058b
Kelig MARTIN <kmartin@wedigital.garden>
2021-07-06 09:59:09 +0200

    fix(beer): remove useless condition

--- src/main/java/fr/esgi/cocotton/beer/infrastructure/error/RestTemplateResponseErrorHandler.java
+++ src/main/java/fr/esgi/cocotton/beer/infrastructure/error/RestTemplateResponseErrorHandler.java
@@ -24,15 +24,8 @@ public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
     public void handleError(ClientHttpResponse httpResponse)
             throws IOException {
 
-        if (httpResponse.getStatusCode()
-                .series() == HttpStatus.Series.SERVER_ERROR) {
-            // handle SERVER_ERROR
-        } else if (httpResponse.getStatusCode()
-                .series() == HttpStatus.Series.CLIENT_ERROR) {
-            // handle CLIENT_ERROR
-            if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new BeerNotFoundException();
-            }
+        if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+            throw new BeerNotFoundException();
         }
     }
 }
