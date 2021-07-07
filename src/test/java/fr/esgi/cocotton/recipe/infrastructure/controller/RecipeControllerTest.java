31fb4db5dd583c25d4ddcb1c2f38c1aace8ecb53
valle <benoit.valleea@hotmail.fr>
2021-07-07 23:40:31 +0200

    feat(recipe): implement recipe dao tests

--- src/test/java/fr/esgi/cocotton/recipe/infrastructure/controller/RecipeControllerTest.java
+++ src/test/java/fr/esgi/cocotton/recipe/infrastructure/controller/RecipeControllerTest.java
@@ -8,10 +8,12 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import static org.mockito.Mockito.verify;
 
+@SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class RecipeControllerTest {
     @InjectMocks
